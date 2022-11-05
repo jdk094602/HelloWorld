15bd436d3f19dc24b255287cfd901b61c507f1af
Gen <jdk094602@gmail.com>
2022-11-05 13:26:27 +0300

    only eng

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, мой crazy world!");
+        System.out.println("Hello, my crazy world!");
     }
 }
